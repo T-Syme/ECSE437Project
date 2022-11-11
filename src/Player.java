4e0709f3fc6173cea6d0aba622d2d71c68eeeb70
Hana Gustyn <hana.gustyn@mail.mcgill.ca>
2022-11-11 13:49:09 -0500

    Initial fields and methods created for Player class

---
+++ src/Player.java
@@ -0,0 +1,25 @@
+public class Player {
+    private String playerName;
+    private int numOfWins;
+
+    public Player(String playerName) {
+        this.playerName = playerName;
+        this.numOfWins = 0;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public int getNumOfWins() {
+        return numOfWins;
+    }
+
+    public void setNumOfWins(int numOfWins) {
+        this.numOfWins = numOfWins;
+    }
+}
