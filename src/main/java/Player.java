c2d4d7f236169bc4e5bd73425f371712e2bb0852
Hana Gustyn <hana.gustyn@mail.mcgill.ca>
2022-11-20 12:43:40 -0500

    Added gameBoard field to player class

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,12 +1,20 @@
-package main.java;
-
 public class Player {
     private String playerName;
     private int numOfWins;
+    private GameBoard gameBoard;
+
+    public GameBoard getGameBoard() {
+        return gameBoard;
+    }
+
+    public void setGameBoard(GameBoard gameBoard) {
+        this.gameBoard = gameBoard;
+    }
 
     public Player(String playerName) {
         this.playerName = playerName;
         this.numOfWins = 0;
+        this.gameBoard = new GameBoard();
     }
 
     public String getPlayerName() {
