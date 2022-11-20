fe040a853e16fe83dbc10c9ce53e2d70a5d2ad14
Hana Gustyn <hana.gustyn@mail.mcgill.ca>
2022-11-20 12:43:17 -0500

    Added test for GameBoard constructor

---
+++ src/test/java/GameBoardTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class GameBoardTest {
+    private GameBoardSquare board[][];
+    private static final int boardSize = 5;
+
+    @BeforeEach
+    void setup() {
+        this.board = new GameBoard().getBoard();
+    }
+
+    @Test
+    void testGameBoard() {
+        for (int row = 0; row < boardSize; row++) {
+            for (int col = 0; col < boardSize; col++) {
+                if (row == 3 && col == 3) {
+                    assertTrue(board[row][col].isCovered());
+                }
+
+                else {
+                    assertFalse(board[row][col].isCovered());
+                }
+            }
+        }
+
+    }
+}
