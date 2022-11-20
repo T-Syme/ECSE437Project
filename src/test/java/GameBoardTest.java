43fef7a58be46974af75c081900e55caa67eaab7
Tyler <70815823+T-Syme@users.noreply.github.com>
2022-11-20 17:21:24 -0500

    Add tests for checkBoard in GameBoard

--- src/test/java/GameBoardTest.java
+++ src/test/java/GameBoardTest.java
@@ -5,12 +5,14 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class GameBoardTest {
+    private GameBoard gameBoard;
     private GameBoardSquare[][] board;
     private static final int BOARD_SIZE = 5;
 
     @BeforeEach
     void setup() {
-        this.board = new GameBoard().getBoard();
+        this.gameBoard = new GameBoard();
+        this.board = this.gameBoard.getBoard();
     }
 
     @Test
@@ -20,12 +22,67 @@ public class GameBoardTest {
                 if (row == BOARD_SIZE / 2 && col == BOARD_SIZE / 2) {
                     assertTrue(board[row][col].isCovered());
                 }
-
                 else {
                     assertFalse(board[row][col].isCovered());
                 }
             }
         }
+    }
 
+    /**
+     * Method: checkBoard()
+     * Verify that it finds a win in a row
+     */
+    @Test
+    void testCheckBoardRow() {
+        for (int col = 0; col < BOARD_SIZE; col++) {
+            this.board[0][col].setCovered(true);
+        }
+        assertTrue(this.gameBoard.checkBoard());
+    }
+
+    /**
+     * Method: checkBoard()
+     * Verify that it finds a win in a column
+     */
+    @Test
+    void testCheckBoardCol() {
+        for (int row = 0; row < BOARD_SIZE; row++) {
+            this.board[row][0].setCovered(true);
+        }
+        assertTrue(this.gameBoard.checkBoard());
+    }
+
+    /**
+     * Method: checkBoard()
+     * Verify that it finds a win in a diagonal (1)
+     */
+    @Test
+    void testCheckBoardDiagonal1() {
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            this.board[i][i].setCovered(true);
+        }
+        assertTrue(this.gameBoard.checkBoard());
+    }
+
+    /**
+     * Method: checkBoard()
+     * Verify that it finds a win in a diagonal (2)
+     */
+    @Test
+    void testCheckBoardDiagonal2() {
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            this.board[i][BOARD_SIZE - 1 - i].setCovered(true);
+        }
+        assertTrue(this.gameBoard.checkBoard());
+    }
+
+    /**
+     * Method: checkBoard()
+     * Verify that it does not find a win
+     */
+    @Test
+    void testCheckBoardNoWin() {
+        assertFalse(this.gameBoard.checkBoard());
     }
 }
