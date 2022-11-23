9f5a3dab3ff809a35cacc3204a00beec33f84c96
Hana Gustyn <hana.gustyn@mail.mcgill.ca>
2022-11-23 11:21:00 -0500

    Created game class to store game results

---
+++ src/main/java/Game.java
@@ -0,0 +1,20 @@
+public class Game {
+    private Player winner;
+    private int numOfPlayers;
+
+    public Player getWinner() {
+        return winner;
+    }
+
+    public void setWinner(Player winner) {
+        this.winner = winner;
+    }
+
+    public int getNumOfPlayers() {
+        return numOfPlayers;
+    }
+
+    public void setNumOfPlayers(int numOfPlayers) {
+        this.numOfPlayers = numOfPlayers;
+    }
+}
