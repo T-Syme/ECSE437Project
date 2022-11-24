235cf32f9f936ff32306662ded236e0ea5a9ab12
Hana Gustyn <hana.gustyn@mail.mcgill.ca>
2022-11-23 19:09:46 -0500

    Added more output written to provide the user with more information

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Game {
     private static final int BOARD_SIZE = 5;
@@ -25,8 +26,14 @@ public class Game {
 
     public void playGame() {
         RandomGenerator generator = new RandomGenerator();
+        for (Player player : players) {
+            System.out.println("\n" + player.getPlayerName() + "'s board is:");
+            player.getGameBoard().printBoard();
+        }
+
         while (true) {
             RandomValue value = null;
+
             try {
                 value = generator.getNextValue();
             } catch (Exception e) {
@@ -34,13 +41,17 @@ public class Game {
                 System.exit(1);
             }
 
+            int col = value.getColumn();
+            System.out.println("The Bingo value is: " + getBingoColumnLetter(col) + value.getNumber());
+
             for (Player player : players) {
                 GameBoard board = player.getGameBoard();
-                int col = value.getColumn();
 
                 for (int row = 0; row < BOARD_SIZE; row++) {
                     if (board.getBoard()[row][col].getValue() == value.getNumber()) {
+                        System.out.println(player.getPlayerName() + " has a match!");
                         board.getBoard()[row][col].setCovered(true);
+                        break;
                     }
                 }
 
@@ -48,13 +59,36 @@ public class Game {
                     addWinner(player);
                 }
 
-                System.out.println(player.getPlayerName() + "'s board\n");
+                System.out.println("\n" + player.getPlayerName() + "'s board");
                 board.printBoard();
             }
 
             if (this.winners.size() != 0) {
                 break;
             }
+        }
+
+        System.out.println("The winners are:");
+
+        for (Player player : winners) {
+            System.out.println(player.getPlayerName());
+        }
+    }
+
+    private String getBingoColumnLetter(int col) {
+        switch(col) {
+            case 0:
+                return "B";
+            case 1:
+                return "I";
+            case 2:
+                return "N";
+            case 3:
+                return "G";
+            case 4:
+                return "O";
+            default:
+                return "";
         }
     }
 }
